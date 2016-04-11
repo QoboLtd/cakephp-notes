4f6aa514fd694c295f3bcddb80c9e56b8f501307
George Constantinou <g.constantinou@qobo.biz>
2016-04-11 17:49:23 +0300

    plugin migrations (task #1722)

---
+++ config/Migrations/20160411144037_CreateNotes.php
@@ -0,0 +1,64 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateNotes extends AbstractMigration
+{
+
+    public $autoId = false;
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('notes');
+        $table->addColumn('id', 'uuid', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('title', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('type', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('user_id', 'uuid', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('model', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('shared', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('content', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addPrimaryKey([
+            'id',
+        ]);
+        $table->create();
+    }
+}
