4a613887ce2d1664069d914bc04d7ddda8518a92
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-10-15 17:04:16 +0300

    Added some more unit tests

--- tests/TestCase/Model/Table/NotesTableTest.php
+++ tests/TestCase/Model/Table/NotesTableTest.php
@@ -80,4 +80,36 @@ class NotesTableTest extends TestCase
     {
         $this->markTestIncomplete('Not implemented yet.');
     }
+
+    public function testGetTypes()
+    {
+        $result = $this->Notes->getTypes();
+        $this->assertTrue(is_array($result), "getTypes() returns a non-array");
+        $this->assertFalse(empty($result), "getTypes() returns an empty result");
+        $this->assertArrayHasKey('warning', $result, "'warning' is not in returned types");
+        $this->assertArrayHasKey('info', $result, "'info' is not in returned types");
+        $this->assertArrayHasKey('danger', $result, "'danger' is not in returned types");
+        $this->assertArrayHasKey('success', $result, "'success' is not in returned types");
+    }
+
+    public function testGetShared()
+    {
+        $result = $this->Notes->getShared();
+        $this->assertTrue(is_array($result), "getShared() returns a non-array");
+        $this->assertFalse(empty($result), "getShared() returns an empty result");
+        $this->assertContains('Private', $result, "'Private' is not in returned shared values");
+        $this->assertContains('Public', $result, "'Public' is not in returned shared values");
+    }
+
+    public function testGetPublicShared()
+    {
+        $result = $this->Notes->getPublicShared();
+        $this->assertEquals('public', $result);
+    }
+
+    public function testGetPrivateShared()
+    {
+        $result = $this->Notes->getPrivateShared();
+        $this->assertEquals('private', $result);
+    }
 }
