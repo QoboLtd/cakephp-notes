cdbdd7480f522f07d07c6319857a41b4c45ec521
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-10-30 13:02:38 +0200

    Changed Foobar to Notes

--- tests/Fixture/UsersFixture.php
+++ tests/Fixture/UsersFixture.php
@@ -1,5 +1,5 @@
 <?php
-namespace Foobar\Test\Fixture;
+namespace Notes\Test\Fixture;
 
 use Cake\TestSuite\Fixture\TestFixture;
 
--- tests/config/routes.php
+++ tests/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-namespace Foobar\Test\App\Config;
+namespace Notes\Test\App\Config;
 
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
