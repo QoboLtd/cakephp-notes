7f952bf2430b493afd4843bfc188b62fb5456e51
George Constantinou <g.constantinou@qobo.biz>
2016-12-30 12:19:53 +0200

    remove unused View (task #3151)

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -67,34 +67,6 @@ class NotesController extends AppController
     }
 
     /**
-     * View method
-     *
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     * @param string|null $id Note id.
-     * @return void
-     */
-    public function view($id = null)
-    {
-        $note = $this->Notes->get($id, [
-            'contain' => ['Users']
-        ]);
-
-        $sharedPrivate = $this->Notes->getPrivateShared();
-        /*
-        if note is private and current user is not the owner, throw exception.
-         */
-        if ($note->shared === $sharedPrivate && $note->user_id !== $this->Auth->user('id')) {
-            throw new UnauthorizedException();
-        }
-
-        $types = $this->Notes->getTypes();
-        $shared = $this->Notes->getShared();
-
-        $this->set(compact('note', 'types', 'shared'));
-        $this->set('_serialize', ['note']);
-    }
-
-    /**
      * Add method
      *
      * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
