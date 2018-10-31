ab249452620ce5e539315fc27cf5f361ca69e133
Leonid Mamchenkov <leonid@mamchenkov.net>
2018-10-31 17:25:34 +0200

    Fix more CakePHP 3.6 deprecated errors (task #8244)

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -11,7 +11,7 @@
  */
 namespace Notes\Controller;
 
-use Cake\Network\Exception\UnauthorizedException;
+use Cake\Http\Exception\UnauthorizedException;
 use Notes\Controller\AppController;
 
 /**
@@ -79,7 +79,7 @@ class NotesController extends AppController
     /**
      * Add method
      *
-     * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
+     * @return \Cake\Http\Response|void Redirects on successful add, renders view otherwise.
      */
     public function add()
     {
@@ -111,8 +111,8 @@ class NotesController extends AppController
      * Edit method
      *
      * @param string|null $id Note id.
-     * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     * @return \Cake\Http\Response|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Http\Exception\NotFoundException When record not found.
      */
     public function edit($id = null)
     {
@@ -146,7 +146,7 @@ class NotesController extends AppController
      * Delete method
      *
      * @param string|null $id Note id.
-     * @return \Cake\Network\Response|null Redirects to index.
+     * @return \Cake\Http\Response|null Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function delete($id = null)
